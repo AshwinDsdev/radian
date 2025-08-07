107fc6be51107f26b129fc6c0e3657c1745ea33b
AshwinDSdev <ashwinraj@datasense.in>
2025-08-07 23:58:59 +0530

    reverted local loan check

--- radian_change_loan/loan_number_filter.js
+++ radian_change_loan/loan_number_filter.js
@@ -58,29 +58,29 @@ async function waitForListener(maxRetries = 20, initialDelay = 100) {
 /**
  * Request a batch of numbers from the storage script
  */
-// async function checkNumbersBatch(numbers) {
-//   return new Promise((resolve, reject) => {
-//     chrome.runtime.sendMessage(
-//       EXTENSION_ID,
-//       {
-//         type: "queryLoans",
-//         loanIds: numbers,
-//       },
-//       (response) => {
-//         if (chrome.runtime.lastError) {
-//           return reject(chrome.runtime.lastError.message);
-//         } else if (response.error) {
-//           return reject(response.error);
-//         }
+async function checkNumbersBatch(numbers) {
+  return new Promise((resolve, reject) => {
+    chrome.runtime.sendMessage(
+      EXTENSION_ID,
+      {
+        type: "queryLoans",
+        loanIds: numbers,
+      },
+      (response) => {
+        if (chrome.runtime.lastError) {
+          return reject(chrome.runtime.lastError.message);
+        } else if (response.error) {
+          return reject(response.error);
+        }
 
-//         const available = Object.keys(response.result).filter(
-//           (key) => response.result[key]
-//         );
-//         resolve(available);
-//       }
-//     );
-//   });
-// }
+        const available = Object.keys(response.result).filter(
+          (key) => response.result[key]
+        );
+        resolve(available);
+      }
+    );
+  });
+}
 
 // ########## DO NOT MODIFY THESE LINES - END ##########
 
@@ -97,51 +97,6 @@ let tableDetectionTimer = null;
 let isTableDetected = false;
 let currentUrl = window.location.href;
 let tablePresenceCheckTimer = null;
-
-const LoanNums = [
-  "0194737052",
-  "0151410206",
-  "0180995748",
-  "0000000612",
-  "0000000687",
-  "0000000711",
-  "0000000786",
-  "0000000927",
-  "0000000976",
-  "0194737052",
-  "0000001180",
-  "0000001230",
-  "0151410206",
-  "0000001453",
-  "0000001537",
-  "0000001594",
-  "0000001669",
-  "0000001677",
-  "0000001719",
-  "0000001792",
-  "0000001834",
-  "0000001891",
-  "0000002063",
-  "0180995748",
-  "0000002352",
-  "0000002410",
-  "0000002436",
-  "0000002477",
-  "0000002485",
-  "0000002493",
-  "0000002535",
-  "0000002550",
-  "0000002600",
-  "0000002642",
-  "0000002667",
-  "0000002691",
-];
-
-const checkNumbersBatch = async (numbers) => {
-  const available = numbers.filter((num) => LoanNums.includes(num));
-  return available;
-};
-
 /**
  * Check if URL has changed
  */
@@ -684,7 +639,7 @@ function setupTableObserver() {
     console.log("✅ DOM Ready - Initializing filter...");
     try {
       // Check Loan extension connection
-      // await waitForListener();
+      await waitForListener();
 
       // Setup URL change detection first
       console.log("Setting up URL change detection...");
